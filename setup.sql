d503abaccfc6ed0df4b0a3a81955265e1b2bec92
Itumeleng-Koboekae <eakeobok03@gmail.com>
2025-03-25 15:18:06 +0200

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -1,4 +1,4 @@
--- Create Database if it doesn’t exist
+-- Create Database if it doesn't exist
 IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'AutoDB')
 BEGIN
     CREATE DATABASE AutoDB;
@@ -10,14 +10,14 @@ USE AutoDB;
 GO
 
 -- Create or update Stored Procedure
-IF OBJECT_ID('dbo.SetupAutoDB', 'P') IS NOT EXISTS
+IF OBJECT_ID('dbo.SetupAutoDB', 'P') IS NOT NULL
     DROP PROCEDURE dbo.SetupAutoDB;
 GO
 
 CREATE PROCEDURE dbo.SetupAutoDB
 AS
 BEGIN
-    -- Create Table if it doesn’t exist
+    -- Create Table if it doesn't exist
     IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Users')
     BEGIN
         CREATE TABLE Users (
